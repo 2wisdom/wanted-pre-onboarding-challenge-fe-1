e493a5d532b9f74cac7b9baa3c7fda769df2faac
Lizzie <leejihye7117@gmail.com>
2022-12-27 21:44:59 +0900

    navbar Link 주석처리

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Navigaiter } from "../styles/GlobalStyle";
 
 export default function Navbar() {
   return (
     <Navigaiter>
       <ul>
-        <li>회원가입</li>
+        <li>
+          {/* <Link to="signup"> */}
+          회원가입
+          {/* </Link> */}
+        </li>
 
-        <li>로그인</li>
+        <li>
+          {/* <Link to="signin"> */}
+          로그인
+          {/* </Link> */}
+        </li>
       </ul>
     </Navigaiter>
   );
