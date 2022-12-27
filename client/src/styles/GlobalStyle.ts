099e2d33592f00b3ddf73c955adf1b848e527768
Lizzie <leejihye7117@gmail.com>
2022-12-27 14:00:29 +0900

    navbar 레이아웃 수정

--- client/src/styles/GlobalStyle.ts
+++ client/src/styles/GlobalStyle.ts
@@ -24,12 +24,14 @@ export const Wrapper = styled.div`
 
 /* Navbar style */
 export const Navigaiter = styled.div`
+  max-width: 1800px;
+  margin: 20px 50px 0 50px;
   ul {
     display: flex;
     justify-content: right;
   }
   li {
     list-style: none;
-    padding-left: 10px;
+    padding: 0 10px 0 10px;
   }
 `;
