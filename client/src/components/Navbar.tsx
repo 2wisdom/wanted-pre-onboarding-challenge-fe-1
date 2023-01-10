7a7ca3009c862eb279003d2a0e29bdda3f1f130e
Lizzie <leejihye7117@gmail.com>
2023-01-10 20:02:46 +0900

    signout 구현

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,24 +1,40 @@
 import React from "react";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Navigaiter } from "../styles/GlobalStyle";
 
 export default function Navbar() {
+  /* Login 상태 */
+  const onLogin = !!localStorage.getItem("Token");
+  /* useNavigate */
+  const navigate = useNavigate();
+
+  /* 로그아웃 */
+  const signOut = () => {
+    localStorage.removeItem("Token");
+    alert("다음에 또 만나요!");
+    navigate("/");
+  };
+
   return (
     <Navigaiter>
       <div>
-        <Link to="todos">
-          <h1>Todo</h1>
+        <Link to="/">
+          <h1>Main</h1>
         </Link>
       </div>
-      <div>
-        <Link to="users/create">회원가입</Link>
-        <Link to="users/login">로그인</Link>
-      </div>
+      {onLogin ? (
+        <div>
+          <Link to="todos">MY Todo</Link>
+          <Link to="/" onClick={signOut}>
+            로그아웃
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <Link to="users/create">회원가입</Link>
+          <Link to="users/login">로그인</Link>
+        </div>
+      )}
     </Navigaiter>
   );
 }
