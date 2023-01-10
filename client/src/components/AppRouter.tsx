dcfea5b182c1df40b9d72e2e2aaf4d9e7e84389f
Lizzie <leejihye7117@gmail.com>
2023-01-10 20:03:37 +0900

    main page 생성

--- client/src/components/AppRouter.tsx
+++ client/src/components/AppRouter.tsx
@@ -4,11 +4,13 @@ import SignUp from "../pages/auth/SignUp";
 import SignIn from "../pages/auth/SignIn";
 import TodoList from "../pages/todos/TodoList";
 import TodoDetail from "../pages/todos/TodoDetail";
+import Index from "../pages";
 
 export default function AppRouter() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Index />} />
         <Route path="/users/create" element={<SignUp />} />
         <Route path="/users/login" element={<SignIn />} />
         <Route path="/todos" element={<TodoList />} />
